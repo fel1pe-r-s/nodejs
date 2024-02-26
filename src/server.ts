3d40b882aafbac99054952b42e3741a1389cf7d9
fel1pe-r-s <1felipersilva1@gmail.com>
2024-02-25 22:26:17 -0300

    configurações de produção

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import { env } from "./env";
 // Run the server!
 const start = async () => {
   try {
-    await app.listen({ port: env.PORT });
+    await app.listen({
+      port: env.PORT,
+      host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
+    });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
